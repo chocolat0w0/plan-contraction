f61e9a7da59fe5c9cb2673cd406f56ee737925b2
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2015-07-08 21:47:27 +0900

    00回等

---
+++ src/model/PlanEntity.scala
@@ -0,0 +1,27 @@
+package model
+
+/**
+ * Created by shiori on 2015/07/08.
+ */
+abstract class PlanEntity {
+  def name:String
+  def fee:Int
+  def capacity:String
+}
+
+object PlanEntity {
+  def entity(str:String) = {
+    str match {
+      case "small" => new SmallPlanEntity
+      case "normal" => new NormalPlanEntity
+      case "large" => new LargePlanEntity
+      case _ => throw new NoPlanException
+    }
+  }
+
+  def message(plan: PlanEntity) = {
+    "選択したのは%sで月%d円、容量は%sです。".format(plan.name, plan.fee, plan.capacity)
+  }
+}
+
+class NoPlanException extends Exception
---
+++ src/model/SmallPlanEntity.scala
@@ -0,0 +1,10 @@
+package model
+
+/**
+ * Created by shiori on 2015/07/08.
+ */
+class SmallPlanEntity extends PlanEntity {
+  def name = "スモールプラン"
+  def fee = 3000
+  def capacity = "1GB"
+}
