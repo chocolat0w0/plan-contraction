a1d21f2e526ea3fd9e6b2cbf3a916849313ff032
chocolat0w0 <shiori.yamazaki.1002@gmail.com>
2015-07-09 22:09:33 +0900

    04回答：プラン変更

--- src/model/account/AccountEntity.scala
+++ src/model/account/AccountEntity.scala
@@ -1,8 +1,13 @@
 package model.account
 
+import model.plan.PlanEntity
+
 abstract class AccountEntity {
   val discount:Int
   def say():Unit
+  def changePlan(current:PlanEntity, next:PlanEntity):Unit = {
+    println("%sから%sに変更しました。".format(current.name, next.name))
+  }
 }
 
 object AccountEntity {
