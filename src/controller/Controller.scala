b9da893a4ce0fe71ec7a467fc27e91a25fb591e2
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2015-07-09 22:09:33 +0900

    04回答：プラン変更

--- src/controller/Controller.scala
+++ src/controller/Controller.scala
@@ -12,14 +12,18 @@ object Controller {
   def main (args: Array[String]) {
     println("your account? normal/premium")
     val accountStr = Source.stdin.getLines().toSeq.head
-    println("plan? small/normal/large(/mega)")
-    val planStr = Source.stdin.getLines().toSeq.head
+    println("current plan? small/normal/large(/mega)")
+    val currentPlanStr = Source.stdin.getLines().toSeq.head
+    println("next plan? small/normal/large(/mega)")
+    val nextPlanStr = Source.stdin.getLines().toSeq.head
 
     try {
       val account = AccountEntity.entity(accountStr)
-      val plan = PlanEntity.entity(planStr, account)
+      val currentPlan = PlanEntity.entity(currentPlanStr, account)
+      val nextPlan = PlanEntity.entity(nextPlanStr, account)
       account.say()
-      plan.say(account)
+      account.changePlan(currentPlan, nextPlan)
+      nextPlan.say(account)
     } catch {
       case e:NoPlanException => println("プラン名が間違っています。")
       case e:NoAccountException => println("アカウント名が間違っています。")
